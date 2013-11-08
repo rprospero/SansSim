a2c84dc37259a31182b5ad134ef7e8ad1442f96a
Adam Washington <adlwashi@indiana.edu>
2013-11-07 20:42:45 -0500

    Added simple q-range function.

--- src/formfactor.js
+++ src/formfactor.js
@@ -1,3 +1,13 @@
+qrange = function (min,max,steps) {
+    var list = [];
+    var i = 0;
+    step = (max-min)/(steps-1);
+    for(i=0;i<steps;i++){
+	list[i] = min + step*i;
+    }
+    return list;
+}
+
 hardSphere = function (q,scale,radius,drho,background) {
     var xrad = q*radius;
     var bes = 3.0*(Math.sin(xrad)-xrad*Math.cos(xrad))/Math.pow(xrad,3);
